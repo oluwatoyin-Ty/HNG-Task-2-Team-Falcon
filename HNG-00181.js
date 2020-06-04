c1182cf6db71f87c1990108b0d1ea033fd260359
oluwatoyin-Ty <66219838+oluwatoyin-Ty@users.noreply.github.com>
2020-06-04 19:58:15 +0100

    Add files via upload

---
+++ HNG-00181.js
@@ -0,0 +1,6 @@
+var name = "Oluwatoyin Bakare"
+var id = "HNG-00181"
+var email = "futoyinbakare@gmail.com"
+
+console.log("Hello World, " + "This is " + name + " with HNGi7 ID " + id + " using Javascript for stage 2 task. " + email)
+
